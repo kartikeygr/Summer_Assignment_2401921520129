b161775394c9536569e090202c07b16ebc56d763
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-02 20:07:54 +0530

    Fix formatting and spacing in maxSubArray method

--- Week_1/Day_2/MaximumSubarray.java
+++ Week_1/Day_2/MaximumSubarray.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int currSum=0; 
         int maxSum=Integer.MIN_VALUE;
-        for(int val: nums){
+        for(int val:nums){
             currSum+=val;
             maxSum=Math.max(currSum,maxSum);
-
+// reset if sum becomes negative
             if(currSum<0){
                 currSum=0;
             }
