118a5d5609355b537a0c48ce22ee877d2bcffcdb
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-15 22:56:01 +0530

    Create LinkedListCycle.java

---
+++ Week_3/Day_1/LinkedListCycle.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            // cycle found
+            if(slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
