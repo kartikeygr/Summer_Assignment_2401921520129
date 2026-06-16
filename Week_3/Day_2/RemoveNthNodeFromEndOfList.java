46dd993d033aa03c85a64a0e3afcd64027a499c8
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-16 21:52:44 +0530

    Implement removeNthFromEnd method in Solution class

---
+++ Week_3/Day_2/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+       if(head.next==null){
+        return null;
+       } 
+       //size
+       int size=0;
+       ListNode curr=head;
+       while(curr!=null){
+        curr=curr.next;
+        size++;
+       }
+
+       if(n==size){
+        return head.next;
+       }
+//size-n is like run from start till the node whose next node has to be delete.
+       int indexToSearch = size - n;
+       ListNode prev=head;
+       int i=1;
+       while(i < indexToSearch){
+        prev=prev.next;
+        i++;
+       }
+       prev.next=prev.next.next;
+       return head;
+    }
+}
