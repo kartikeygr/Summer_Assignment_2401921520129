bd8aca49085cdebcc374ad6fb7e73af0db638b2c
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-23 22:49:58 +0530

    Create BinaryTreeInorderTraversel.java

---
+++ Week_4/Day_2/BinaryTreeInorderTraversel.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<>();
+        inorder(root,output);
+        return output;
+    }
+
+    public void inorder(TreeNode root, List<Integer> output) {
+        // base case
+        if(root==null) {
+            return;
+        }
+        // left
+        inorder(root.left,output);
+        // root
+        output.add(root.val);
+        // right
+        inorder(root.right,output);
+    }
+}
