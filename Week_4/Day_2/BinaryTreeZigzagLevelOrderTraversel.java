36fd90e0abb35a57a50d890f80d2194c4496e7a6
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-23 22:43:14 +0530

    Create BinaryTreeZigzagLevelOrderTraversel.java

---
+++ Week_4/Day_2/BinaryTreeZigzagLevelOrderTraversel.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        boolean leftToRight=true;
+        while(!que.isEmpty()) {
+
+            int s=que.size();
+
+            List<Integer> level = new ArrayList<>();
+            for(int i=0;i<s;i++){
+                TreeNode current=que.poll();
+
+                // normal order
+                if(leftToRight) {
+                    level.add(current.val);
+                } else {
+                    // reverse order
+                    level.add(0, current.val);
+                }
+                if(current.left !=null) {
+                    que.offer(current.left);
+                }
+                if(current.right !=null) {
+                    que.offer(current.right);
+                }
+            }
+            ans.add(level);
+            // change direction
+            leftToRight = !leftToRight;
+        }
+
+        return ans;
+    }
+}
