de10d605fdb3927eb02dbcbea5552393e1194678
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-23 22:59:17 +0530

    Implement level order traversal for binary tree

---
+++ Week_4/Day_2/BinaryTreeLevelOrderTraversel.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+
+        // empty tree
+        if(root==null){
+            return ans;
+        }
+
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int s=q.size();
+
+            List<Integer> level = new ArrayList<>();
+            // process current level
+            for(int i=0;i<s;i++){
+                TreeNode curr=q.poll();
+                level.add(curr.val);
+
+                // add left child
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+
+                // add right child
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
