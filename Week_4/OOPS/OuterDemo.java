833fda6dcfff92ede48736c69a160a934a1f2586
Kartikey Gangwar <kartikgangwar314@gmail.com>
2026-06-27 22:05:44 +0530

    Add Outer and Inner class demonstration

---
+++ Week_4/OOPS/OuterDemo.java
@@ -0,0 +1,21 @@
+class Outer {
+
+    void display() {
+
+        System.out.println("Outer class display");
+    }
+    class Inner {
+        void display() {
+            System.out.println("Inner class display");
+        }
+    }
+}
+public class OuterDemo {
+
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        o.display();
+        Outer.Inner i = o.new Inner();
+        i.display();
+    }
+}
